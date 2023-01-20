da69655a6fd229a466227823d862bac831e9618f
binaryyoung <asqw4545@gmail.com>
2023-01-21 03:28:19 +0900

    프로젝트 세팅

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# shoeplace
+# 👟 ShoePlace
+
+한정판 신발 거래 중계 서비스 ShoePlace 입니다.
+
+## 🖥️ 사용기술
+
+- java 11
+- Spring Boot
+- JUnit5
+- MySQL 8.0
+- JPA
---
+++ src/test/java/com/shoeplace/ShoeplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoeplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
