8dc06189d01c839829fbc5855ba21b77c08ed786
binaryyoung <asqw4545@gmail.com>
2023-02-16 13:43:47 +0900

    feat : 브랜드 조회 기능 추가

---
+++ src/main/java/com/shoeplace/dto/BrandInfoDto.java
@@ -0,0 +1,37 @@
+package com.shoeplace.dto;
+
+import javax.validation.constraints.NotNull;
+
+import com.shoeplace.entity.Brand;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class BrandInfoDto {
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Request {
+		@NotNull
+		private String name;
+
+		@NotNull
+		private Integer page;
+	}
+
+	@Builder
+	@Getter
+	public static class Response {
+		private String korName;
+		private String engName;
+
+		public static Response of(Brand brand) {
+			return Response.builder()
+				.korName(brand.getKorName())
+				.engName(brand.getEngName())
+				.build();
+		}
+	}
+}
